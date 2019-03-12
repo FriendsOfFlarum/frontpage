4352a01aadd7ae37198dd0af884889f635ddcc0f
Christian Lopez <reflardevs@gmail.com>
2019-03-12 11:02:03 -0400

    Move Push/Pull to DiscussionControls instead of PostControls

--- js/src/forum/addFrontPage.js
+++ js/src/forum/addFrontPage.js
@@ -1,5 +1,5 @@
 import { extend } from 'flarum/extend';
-import PostControls from 'flarum/utils/PostControls';
+import DiscussionControls from 'flarum/utils/DiscussionControls';
 import Button from 'flarum/components/Button';
 import Model from 'flarum/Model';
 import Discussion from 'flarum/models/Discussion';
@@ -8,16 +8,16 @@ Discussion.prototype.frontpage = Model.attribute('frontpage');
 Discussion.prototype.front = Model.attribute('front');
 
 export default function addFrontPage() {
-  extend(PostControls, 'moderationControls', function(items, post) {
-    let isFront = post.discussion().frontpage();
+  extend(DiscussionControls, 'moderationControls', function(items, discussion) {
+    let isFront = discussion.frontpage();
 
-    if (post.discussion().front()) {
+    if (discussion.front()) {
       items.add('frontpage', Button.component({
-        children: app.translator.trans(post.discussion().frontpage() ? 'core.forum.post_controls.pull_from_front_button' : 'core.forum.post_controls.push_to_front_button'),
+        children: app.translator.trans(discussion.frontpage() ? 'core.forum.post_controls.pull_from_front_button' : 'core.forum.post_controls.push_to_front_button'),
         icon: 'fas fa-home',
         onclick: () => {
           isFront = !isFront;
-          post.discussion().save({frontpage: isFront});
+          discussion.save({frontpage: isFront});
         }
       }));
     }
